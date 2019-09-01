e1a19c3523d4cf61366dddcf521cc7169dd607b6
Adrian Cieplinski <a.cieplinski@gmail.com>
2019-09-01 23:28:00 +0200

    RecipeService serves as a cross-component communicator

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import {Recipe} from './recipe.model';
+import {EventEmitter} from '@angular/core';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is simply a test',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhVCuSzGC189ACUZbxCZfgoPS6s0E_tdkK0meKElud6uYBWjf4Uw'),
